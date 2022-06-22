b21b80f94c694e9ddf50f62b8d47ff7a503e4f07
oliveryh <oyerburyhodgson@gmail.com>
2022-06-22 23:12:37 +0100

    :card_file_box: migration: store article completion as datetime (#32)

--- client/pandora/tasks/filters.py
+++ client/pandora/tasks/filters.py
@@ -1,8 +1,9 @@
 import django_filters
+
 from pandora.tasks.models import Article
 
 
 class ArticleListFilter(django_filters.FilterSet):
     class Meta:
         model = Article
-        fields = ['is_complete']
+        fields = ['completed_at']
